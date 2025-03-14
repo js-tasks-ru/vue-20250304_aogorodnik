22df4d58f04d579c69e41338530619b63385474c
Андрей Огородник <aogorodnik@gmail.com>
2025-03-14 14:33:31 +0100

    Решение задачи 01-basics\10-create-app, использование функции

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,10 +1,22 @@
 import { defineComponent, createApp } from 'vue'
 
+function formatAsLocalDate() {
+    return new Date().toLocaleString(
+        navigator.language, { dateStyle: 'long' }
+    )
+}
+
 const App = defineComponent({
     name: 'App',
 
+    setup() {
+        return {
+            formatAsLocalDate
+        }
+    },
+
     template: `
-        Сегодня {{ new Date().toLocaleDateString() }}
+        Сегодня {{ formatAsLocalDate() }}
     `
 })
 
