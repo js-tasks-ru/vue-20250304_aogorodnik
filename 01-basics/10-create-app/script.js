24017284f320a605db612a043fa19c992d047db4
Андрей Огородник <aogorodnik@gmail.com>
2025-03-14 13:45:07 +0100

    Решение задачи 01-basics\10-create-app

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,15 @@
 import { defineComponent, createApp } from 'vue'
+
+const App = defineComponent({
+    name: 'App',
+
+    template: `
+        Сегодня {{ new Date().toLocaleDateString() }}
+    `
+})
+
+const app = createApp(App)
+
+const vm = app.mount('#app')
+
+window.vm = vm
