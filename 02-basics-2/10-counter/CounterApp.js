060c622a19b7843b2d47c32d4e1c448db2fce040
Андрей Огородник <aogorodnik@gmail.com>
2025-03-23 10:12:11 +0100

    Решение 02-basics-2\10-counter

--- 02-basics-2/10-counter/CounterApp.js
+++ 02-basics-2/10-counter/CounterApp.js
@@ -1,9 +1,33 @@
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'CounterApp',
 
-  setup() {},
+  setup() {
+
+    const counter = ref(0)
+    const step = 1
+
+    function changeCounter(value) {
+      counter.value += value
+    }
+
+    function isMinAchived() {
+      return counter.value <= 0
+    }
+
+    function isMaxAchived() {
+      return counter.value >= 5
+    }
+  
+    return {
+      counter,
+      step,
+      changeCounter,
+      isMinAchived,
+      isMaxAchived
+    }
+  },
 
   template: `
     <div class="counter">
@@ -11,15 +35,18 @@ export default defineComponent({
         class="button button--secondary"
         type="button"
         aria-label="Decrement"
-        disabled
+        :disabled="isMinAchived()"
+        @click="changeCounter(-step)"
       >➖</button>
 
-      <span class="count" data-testid="count">0</span>
+      <span class="count" data-testid="count">{{ counter }}</span>
 
       <button
         class="button button--secondary"
         type="button"
         aria-label="Increment"
+        :disabled="isMaxAchived()"
+        @click="changeCounter(step)"
       >➕</button>
     </div>
   `,
