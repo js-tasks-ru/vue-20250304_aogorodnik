d8661b9c0ed081934e446fd0747bb970f612f382
Андрей Огородник <aogorodnik@gmail.com>
2025-05-04 08:54:41 +0200

    Решение 03-components\20-UiClock

--- 03-components/20-UiClock/UiClock.js
+++ 03-components/20-UiClock/UiClock.js
@@ -1,9 +1,32 @@
-import { defineComponent } from 'vue'
+import { defineComponent, ref, computed, onMounted, onUnmounted } from 'vue'
+
+function formatAsLocalDate() {
+  return new Date().toLocaleString(
+      navigator.language, { timeStyle: 'medium' }
+  )
+}
 
 export default defineComponent({
   name: 'UiClock',
 
-  setup() {},
+  setup() {
 
-  template: `<div class="clock">10:12:02</div>`,
+    const printDate = ref(formatAsLocalDate())
+
+    let intervalId
+    
+    onMounted(() => {
+      intervalId = setInterval(() => {
+        printDate.value = formatAsLocalDate()
+      }, 1000)
+    })
+
+    onUnmounted(() => clearInterval(intervalId))
+
+    return {
+      printDate,
+    }
+  },
+
+  template: `<div class="clock">{{ printDate }}</div>`,
 })
